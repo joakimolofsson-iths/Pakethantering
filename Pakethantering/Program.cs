d426c1a8bc89abf0f8e30e00861e7b1b3be96b6a
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-28 09:48:24 +0100

    Adding project files

---
+++ Pakethantering/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pakethantering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
